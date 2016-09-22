70d617cd7a68d0b6a7f95d4f56de1a2ee8ef4d65
zhx <chengxuyuan001@163.com>
2016-09-22 16:04:58 +0800

    ‘图片合成gif’

--- xgif/xgif.go
+++ xgif/xgif.go
@@ -1,6 +1,8 @@
 package xgif
 
 import (
+	"bytes"
+	"image"
 	"image/gif"
 	"image/jpeg"
 	"log"
@@ -30,7 +32,7 @@ func DecodeGifFrame(gifPath string, outDir string) {
 		log.Fatalln(err, "getAllFrame_DecodeAll")
 	}
 	//	dir, _ := os.Getwd()
-	imgOption := &jpeg.Options{Quality: 50}
+	imgOption := &jpeg.Options{Quality: 100}
 
 	for index, item := range gifData.Image {
 		itempath := path.Join(outDir, strconv.Itoa(index)+"_"+strconv.Itoa(imgOption.Quality)+".jpg")
@@ -48,6 +50,117 @@ func DecodeGifFrame(gifPath string, outDir string) {
 	}
 }
 
-//func EncodeImagesToGif(listimage []*image.Image, outpath string) {
-//	gif.EncodeAll()
-//}
+func EncodeImagesToGif(listimage []*image.Image, outpath string) {
+	log.Println("EncodeImagesToGif")
+	dir := path.Dir(outpath)
+	err := os.MkdirAll(dir, 0666)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	outGif := &gif.GIF{}
+	for _, itemValue := range listimage {
+		gifItemWriter := &bytes.Buffer{}
+
+		gif.Encode(gifItemWriter, *itemValue, nil)
+
+		gifImg, err := gif.Decode(gifItemWriter)
+		if err != nil {
+			log.Println("gif.Decode()", err)
+		}
+		outGif.Image = append(outGif.Image, gifImg.(*image.Paletted))
+		outGif.Delay = append(outGif.Delay, 0)
+		//		gifItemWriter.Reset()
+	}
+
+	outFile, err := os.OpenFile(outpath, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer outFile.Close()
+	err = gif.EncodeAll(outFile, outGif)
+	if err != nil {
+		log.Fatalln("gif.EncodeAll", err)
+	}
+}
+
+func EncodeFileToGif(filelist []string, outpath string) {
+	imageList := make([]*image.Image, 0)
+
+	for _, itemPic := range filelist {
+		itemFile, err := os.Open(itemPic)
+		if err != nil {
+			log.Println("os.Open", err, itemPic)
+			continue
+		}
+		defer itemFile.Close()
+
+		itemImage, _, err := image.Decode(itemFile)
+		if err != nil {
+			log.Println("Decode", err, itemPic)
+			continue
+		}
+
+		imageList = append(imageList, &itemImage)
+	}
+	EncodeImagesToGif(imageList, outpath)
+}
+
+//压缩gif
+func CompressGif(sourceGif string, outGif string, quality int) {
+	sourceFile, err := os.Open(sourceGif)
+	if err != nil {
+		log.Fatalln("Open", err, sourceGif)
+	}
+	defer sourceFile.Close()
+
+	outFile, err := os.OpenFile(outGif, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalln("OpenFile", err, outGif)
+	}
+	defer outFile.Close()
+
+	gifData, err := gif.DecodeAll(sourceFile)
+	if err != nil {
+		log.Fatalln("Decode", err, sourceGif)
+	}
+	outGifData := gif.GIF{}
+	outGifData.Delay = gifData.Delay
+
+	option := &jpeg.Options{quality}
+	//	gifOption := &gif.Options{}
+	//	gifOption.NumColors = 2
+
+	for _, itemImage := range gifData.Image {
+		itemJPG := &bytes.Buffer{}
+		err = jpeg.Encode(itemJPG, itemImage, option)
+		if err != nil {
+			log.Println("jpeg.Encode()", err)
+			continue
+		}
+		gifImage, _, err := image.Decode(itemJPG)
+		if err != nil {
+			log.Println("image.Decode", err)
+		}
+		//image.NewPaletted(gifImage.Bounds(),)
+		itemGif := &bytes.Buffer{}
+		err = gif.Encode(itemGif, gifImage, nil)
+		if err != nil {
+			log.Println("gif.Encode", err)
+		}
+
+		gifFrame, err := gif.Decode(itemGif)
+		if err != nil {
+			log.Println("gif.Decode", err)
+			continue
+		}
+		outGifData.Image = append(outGifData.Image, gifFrame.(*image.Paletted))
+	}
+
+	err = gif.EncodeAll(outFile, &outGifData)
+	if err != nil {
+		log.Fatalln("gif.Encode", err)
+	}
+}
--- xgif/xgif_test.go
+++ xgif/xgif_test.go
@@ -5,5 +5,22 @@ import (
 )
 
 func TestGetAllGifFrame(t *testing.T) {
-	DecodeGifFrame("/media/zhxcxy/win_work/go/code/workspace/src/ximage/doc/test.gif", "")
+	DecodeGifFrame("/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/lol.gif", "")
+}
+
+func TestEncodeFileToGif(t *testing.T) {
+	//	fileList := make([]string, 0)
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/0.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/0_0.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/1.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/1_0.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/2.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/2_0.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/3.jpg")
+	//	fileList = append(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/3_0.jpg")
+	//	EncodeFileToGif(fileList, "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/res.gif")
+}
+
+func TestCompressGif(t *testing.T) {
+	//	CompressGif("/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/lol.gif", "/Users/Mac/Work/go/code/work/src/github.com/xfort/RockImage/xgif/gif/lol_256_100.gif", 50)
 }
